d9c822fa47953001f4d41736c79463cc7758c8aa
Jonathan Alejandro Cordoba Leiton <111367117+Jonathan6432@users.noreply.github.com>
2024-02-09 21:35:15 -0500

    Se crea la clase monitor, computadora y orden, se realiza pruebas y funcionan correctamente, se espera validar pruebas el dìa de mañana con el profesora

--- classComput.js
+++ classComput.js
@@ -1,3 +1,4 @@
+// clase padre
 class DispositivoEntrada {
   constructor(tipoEntrada, marca) {
     this._tipoEntrada = tipoEntrada;
@@ -48,8 +49,10 @@ class Raton extends DispositivoEntrada {
 }
 
 // Prueba para validar que la class Raton esta funcionando correctamente
-let ratonp = new Raton("USB","HP");
-console.log(ratonp.toString());
+let ratonP = new Raton("USB","HP");
+console.log(ratonP.toString());
+
+let raton1 = new Raton("Blutooth","Microsoft");
 
 // -------------------------------------------------
 // incion clase hija teclado
@@ -76,7 +79,167 @@ class Teclado  extends DispositivoEntrada{
 }
 
 // Pruba teclado 
-let Tecladop = new Teclado("USB", "PHP");
-console.log(Tecladop.toString());
+let tecladoP = new Teclado("USB", "PHP");
+console.log(tecladoP.toString());
 
+let teclado1 = new Teclado("Blutooth", "Razer");
 
+// --------------------------------------------
+// Inicio clase independiente - Monitor 
+// ---------------------------------------------
+class Monitor{
+  // atributo estatico permite inicializar y agregar valor unico
+    static contadorMonitores = 0; 
+
+    constructor(marca,tamaño){
+      this._idMonitor = ++Monitor.contadorMonitores;
+
+      this._marca = marca; 
+      this._tamaño = tamaño;
+    }
+    // Se agrega get atributo solo lectura
+    get idMonitor(){
+      return this._idMonitor;
+    }
+
+    get marca(){
+      return this._marca;
+    }
+    
+    get tamaño(){
+      return this._tamaño;
+    }
+
+    set marca(marca){
+      this._marca = marca;
+    }
+
+    set tamaño(tamaño){
+      this._tamaño = tamaño;
+    }
+
+    toString() {
+      return `Id Monitor: #${this.idMonitor}, Marca: ${this.marca}, Tamaño: ${this.tamaño}`;
+  }
+
+}
+
+// Prueba de objeto monitor
+let monitorP = new Monitor ("Asus","27 pulgadas" );
+console.log(monitorP.toString());
+
+let monitor1 = new Monitor ("Dell","18 pulgadas" );
+
+// Fin clase monitor
+
+// ----------------------------------------------------
+// Inicio clase computadora
+// ----------------------------------------------------
+
+class Computadora {
+
+  static contadorComputadoras = 0;
+
+  constructor(nombre,Monitor,Teclado,Raton){
+
+    this._idComputadora = ++Computadora.contadorComputadoras;
+    this._nombre = nombre;
+
+    this._Monitor = Monitor;
+    this._Teclado = Teclado;
+    this._Raton = Raton;
+
+  }
+  // Inicio getters para leer la información
+  get idComputadora(){
+    return this._idComputadora;
+  }
+
+  get nombre(){
+    return this._nombre;
+  }
+
+  get Monitor(){
+    return this._Monitor;
+  }
+  get Teclado(){
+    return this._Teclado;
+  }
+
+  get Raton(){
+    return this._Raton;
+  }
+
+  // Incio setters para modificar la información
+  set nombre(nombre){
+    this._nombre = nombre;
+  }
+  set Monitor(Monitor){
+    this._Monitor = Monitor;
+  }
+  set Teclado(Teclado){
+    this._Teclado = Teclado;
+  }
+  set Raton(Ratos){
+    this._Raton = Raton;
+  }
+
+  toString(){
+    return `Id Computadora: #${this.idComputadora},Nombre: ${this.nombre},\nMonitor: ${this.Monitor},\nTeclado: ${this.Teclado},\nRaton: ${this.Raton}`;
+  }
+ 
+
+}
+// Creo objeto de prueba agregando los atributos de los objetos creados anteriormente.
+let computadoraP = new Computadora ("Asus", monitorP,tecladoP,ratonP);
+
+console.log(computadoraP.toString());
+
+let computadora1 = new Computadora ("Dell", monitor1,teclado1,raton1);
+
+// --------------------------------------------------
+
+// Inicio clase Orden 
+
+class Orden {
+  static contadorOdenes = 0;
+
+  constructor(){
+    this._idOrden = ++Orden.contadorOdenes;
+    this._computadora = [];
+  }
+
+  get idOrden(){
+    return this._idOrden;
+  }
+
+  agregarComputadora(computadora){
+    // metodo push, para agregar productos al arreglo
+    this._computadora.push(computadora);
+
+  }
+
+  mostrarOrden(){
+    // inicia variables vacia para acomular productos
+    let productosOrden = " ";
+
+    for (let computadora of this._computadora){
+      productosOrden += '\n['+computadora.toString() + "]";
+    }
+
+    console.log(`Orden: ${this._idOrden} \n Computadora: ${productosOrden}`);
+}
+
+}
+
+let orden1 = new Orden();
+
+orden1.agregarComputadora(computadoraP);
+orden1.mostrarOrden();
+
+// ----------------------------------------
+
+let orden2 = new Orden();
+
+orden2.agregarComputadora(computadora1);
+orden2.mostrarOrden();
